2ea2ccb15c4f387e2336adcfb96665df7250a004
badrideepak@gmail.com <badrideepak@gmail.com>
2022-04-13 19:53:10 +0100

    Second Commit

--- Python.py
+++ Python.py
@@ -1,7 +1,7 @@
 print("Hello World")
-# print("Welcome to Python Learning")
+print("Welcome to Python Learning")
 a = 10
 b = 20
-# print(a + b)
-# print(a)
-    
+print(a + b)
+print(a)
+print("This is my First program in Visual Studio")
