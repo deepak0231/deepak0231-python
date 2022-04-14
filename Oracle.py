f5dba4f9c19fac4254eb68a2850d3c8e1a6889ef
badrideepak@gmail.com <badrideepak@gmail.com>
2022-04-14 14:23:26 +0100

    Oracle

--- Oracle.py
+++ Oracle.py
@@ -1,5 +1,5 @@
-
 import cx_Oracle
+import pandas as pd
 conn = cx_Oracle.connect('library_test1/library@//localhost:1521/xe')
 print(conn.version)
 
@@ -11,7 +11,10 @@ select * from kchouse
 """
 
 cur.execute(sql_create)
-for line in cur:
-    print(line)
+# for line in cur:
+#     print(line)
 
 print("Table created1")
+df = pd.DataFrame(cur, columns=['id','date','price','bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront','view','condition','grade','sqft_above','sqft_basement','yr_built','yr_renovated','zipcode','lat','long','sqft_living15','sqft_lot15'])
+print(df.head())
+print("Table created2")
