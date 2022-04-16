5810c3bfa8e2d8e186aaca34c59ac9b19d3db952
badrideepak@gmail.com <badrideepak@gmail.com>
2022-04-16 19:47:37 +0100

    New Codes

--- Oracle.py
+++ Oracle.py
@@ -1,5 +1,6 @@
 import cx_Oracle
 import pandas as pd
+import warnings
 conn = cx_Oracle.connect('library_test1/library@//localhost:1521/xe')
 print(conn.version)
 
@@ -13,8 +14,25 @@ select * from kchouse
 cur.execute(sql_create)
 # for line in cur:
 #     print(line)
-
-print("Table created1")
 df = pd.DataFrame(cur, columns=['id','date','price','bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront','view','condition','grade','sqft_above','sqft_basement','yr_built','yr_renovated','zipcode','lat','long','sqft_living15','sqft_lot15'])
-print(df.head())
-print("Table created2")
+# print(df.head())
+# print(df.info())
+# print(df.describe)
+# df.to_excel('Try.xlsx')
+df1 = df[['id','date','price','bedrooms']]
+print(df1.head())
+df2 = df[['bathrooms']]
+# print('df2 is:', df2[0])
+# print(df2[21609])
+# print(df.head(1))
+# warnings.filterwarnings('ignore')
+# df['sqft_living'][0] = 1175
+# print('new:', df.head(1))
+# print(df2.values)
+# print('Sum is:', df2.sum())
+# print(df2.values.sum())
+# print('Mean is:', df2.mean())
+df1['End'] = df1['bedrooms'] * 10
+print(df1.head())
+df1.insert(loc= 2, column='New Col', value= df1['price'] * 2)
+print(df1.head())
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-
-import cx_Oracle
-conn = cx_Oracle.connect('library_test1/library@//localhost:1521/xe')
-print(conn.version)
+print('df2 is:', df2[0])
